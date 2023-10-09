20fb0ca41c4f9b6c6459a07b66cb992f1a4d9f5d
hyejeong <112006114+hyezg@users.noreply.github.com>
2023-10-09 12:09:27 +0900

    Update week03_code.java

--- study/week03_code.java
+++ study/week03_code.java
@@ -20,3 +20,27 @@ public class Ex01 {
 	}
 
 }
+
+//배열 원소의 평균 구하기
+import java.util.Scanner;
+public class Ex02 {
+
+	public static void main(String[] args) {
+		int intArray[];
+		intArray = new int [5];
+		int sum=0;
+		double result=0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요: ");
+		for(int i=0; i<intArray.length; i++) {
+			intArray[i] = sc.nextInt();
+			sum += intArray[i];
+		}
+		
+		result = (double)sum/intArray.length;
+		System.out.println("평균 : "+result);
+	}
+
+}
